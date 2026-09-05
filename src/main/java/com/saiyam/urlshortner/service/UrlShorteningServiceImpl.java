02125d41f6d7149277dd2541c9b79ba3e0f1df00
saiyam7474 <saiyamverma722@gmail.com>
2026-09-05 17:47:52 +0530

    using  generator for short codes

--- src/main/java/com/saiyam/urlshortner/service/UrlShorteningServiceImpl.java
+++ src/main/java/com/saiyam/urlshortner/service/UrlShorteningServiceImpl.java
@@ -2,6 +2,7 @@ package com.saiyam.urlshortner.service;
 
 import com.saiyam.urlshortner.dto.ShortenUrlRequest;
 import com.saiyam.urlshortner.dto.ShortenUrlResponse;
+import com.saiyam.urlshortner.generator.ShortCodeGenerator;
 import com.saiyam.urlshortner.model.UrlMapping;
 import com.saiyam.urlshortner.repository.UrlRepository;
 import org.springframework.stereotype.Service;
@@ -10,14 +11,18 @@ import org.springframework.stereotype.Service;
 public class UrlShorteningServiceImpl implements UrlShorteningService {
 
     private final UrlRepository urlRepository;
+    private final ShortCodeGenerator shortCodeGenerator;
 
-    public UrlShorteningServiceImpl(UrlRepository urlRepository) {
+    public UrlShorteningServiceImpl(
+            UrlRepository urlRepository,
+            ShortCodeGenerator shortCodeGenerator) {
         this.urlRepository = urlRepository;
+        this.shortCodeGenerator = shortCodeGenerator;
     }
 
     @Override
     public ShortenUrlResponse shorten(ShortenUrlRequest request) {
-        String shortCode = "temp123";
+        String shortCode = shortCodeGenerator.generate();
 
         UrlMapping mapping = new UrlMapping(
                 shortCode,
