d232fd7c62953ba823ac3a2ec869e574ea6cd3cf
saiyam7474 <saiyamverma722@gmail.com>
2026-09-05 17:37:30 +0530

    added service to repo connectivity

--- src/main/java/com/saiyam/urlshortner/service/UrlShorteningServiceImpl.java
+++ src/main/java/com/saiyam/urlshortner/service/UrlShorteningServiceImpl.java
@@ -2,13 +2,30 @@ package com.saiyam.urlshortner.service;
 
 import com.saiyam.urlshortner.dto.ShortenUrlRequest;
 import com.saiyam.urlshortner.dto.ShortenUrlResponse;
+import com.saiyam.urlshortner.model.UrlMapping;
+import com.saiyam.urlshortner.repository.UrlRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UrlShorteningServiceImpl implements UrlShorteningService {
 
+    private final UrlRepository urlRepository;
+
+    public UrlShorteningServiceImpl(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
     @Override
     public ShortenUrlResponse shorten(ShortenUrlRequest request) {
-        return new ShortenUrlResponse("TODO");
+        String shortCode = "temp123";
+
+        UrlMapping mapping = new UrlMapping(
+                shortCode,
+                request.url()
+        );
+
+        urlRepository.save(mapping);
+
+        return new ShortenUrlResponse(shortCode);
     }
 }
